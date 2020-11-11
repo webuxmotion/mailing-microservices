e975eb41cd70fa540ef8a0e5c4e62ccca974f370
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2020-11-11 18:12:01 +0200

    typeDefs and resolvers

--- packages/gateway/data/resolvers.js
+++ packages/gateway/data/resolvers.js
@@ -1,5 +1,31 @@
+const mockMails = [
+  {
+    subject: "My First email",
+    receiver: "text@test.com",
+    content: "hello world"
+  },
+  {
+    subject: "My Second email",
+    receiver: "text@test.com",
+    content: "hello world"
+  },
+  {
+    subject: "My Third email",
+    receiver: "text@test.com",
+    content: "hello world"
+  }
+]
+
 module.exports = {
   Query: {
-    hey: () => 'Hey you'
+    mails: () => mockMails,
+    mail: (_, args) => mockMails[0],
+  },
+  Mutation: {
+    mail: (_, args) => {
+      mockMails[0] = args;
+
+      return args;
+    }
   }
 };
--- packages/gateway/data/schema.js
+++ packages/gateway/data/schema.js
@@ -1,9 +1,24 @@
 const { makeExecutableSchema } = require('graphql-tools');
 const { gql } = require('apollo-server-express');
+
 const resolvers = require('./resolvers');
 
 const typeDefs = gql`
-  type Query { hey: String! }
+  type Query { 
+    mails: [Mail]
+    mail(subject: String!, receiver: String!): Mail
+  }
+
+  type Mutation {
+    mail(subject: String!, receiver: String!, content: String!): Mail
+  }
+
+  type Mail {
+    _id: String
+    subject: String
+    receiver: String
+    content: String
+  }
 `;
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers });
